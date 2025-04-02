a5fb45ebfc544e9dec9b4d87c83a2a7303265386
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-04-01 22:02:28 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Header from './components/Header';
+import Card from './components/Card';
+import Footer from './components/Footer';
+import { View } from "react-native";
